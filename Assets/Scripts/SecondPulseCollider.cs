45f93b650fe7e1184073f7411e45e1142164373b
selectedusername <stumpitron@gmail.com>
2012-06-28 10:29:47 +0000

    Added the boxcollider script, so that the backwards pulse can destroy the enemy.

---
+++ Assets/Scripts/SecondPulseCollider.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class SecondPulseCollider : MonoBehaviour {
+	
+	protected void Start () {
+		transform.position = gameObject.transform.parent.transform.position;
+	}
+	
+	protected void OnTriggerExit (Collider otherObject) {
+        if (otherObject.name == "Pulse(Clone)") {
+			if(otherObject.gameObject.GetComponent<LineRenderer>().material.color == gameObject.transform.parent.renderer.material.color) {
+				Player.score += 10;
+            	Player.energy += 5;
+            	if (Player.energy > 50)
+                	Player.energy = 50;
+            	gameObject.transform.parent.GetComponent<EnemyScript>().CreateExplosion();
+            	gameObject.transform.parent.GetComponent<EnemyScript>().DamageEnemy();
+				if (Player.score >= 100)
+                	Application.LoadLevel("Win");
+			} else         //Makes sure it only emits particles when the object is a pulse, not of the same colour
+				 gameObject.transform.parent.GetComponent<ParticleSystem>().Emit(10);	
+		}  
+    }
+	
+}
