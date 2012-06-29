e434f5a347e38a25e06e7cc7964cba9f22acd7bd
Samuel Neugber <yogurt.der.weise@gmail.com>
2012-06-29 16:15:55 +0000

    MP3Decoder codestub. Needs an actual code body!!

---
+++ Assets/Scripts/Sound/MP3Decoder.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class MP3Decoder : DecoderInterface
+{
+	
+	public MP3Decoder(string path) {
+		
+	}
+	
+	public int readSamples(float[] samples) {
+		return 0;
+	}
+}
+
