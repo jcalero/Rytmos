649067089d729e0297154a044cd9c28ecd984ec5
Samuel Neugber <yogurt.der.weise@gmail.com>
2012-07-02 13:51:19 +0000

    This is used when we already have the data (for now)

---
+++ Assets/Scripts/Sound/MockDecoder.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class MockDecoder : DecoderInterface {
+	
+	private float[] data;
+	private int pointer;
+	
+	private MockDecoder() {}
+	
+	public MockDecoder(float[] data) {
+		this.data = data;
+		pointer = 0;
+		
+	}
+	
+	public int readSamples(ref float[] samples) {
+		
+		int readLength = samples.Length;
+		if(pointer + samples.Length >= data.Length) readLength = samples.Length - ((pointer + samples.Length) - data.Length);
+		
+		System.Array.Copy(data,pointer,samples,0,readLength);
+		pointer += readLength;
+		
+		return readLength;
+	}
+}
