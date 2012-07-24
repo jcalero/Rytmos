c8ee2feb5f9ecb7ab90d9b18d5af34bf8dc0cb0a
cpelissier7 <cpelissier7@unknown.com>
2012-07-24 13:59:47 +0000

    Got rid of worthless stuff in Soundtouch library.

--- Assets/Scripts/Sound/SoundTouch.Net/Properties/AssemblyInfo.cs
+++ Assets/Scripts/Sound/SoundTouch.Net/Properties/AssemblyInfo.cs
@@ -18,4 +18,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.6.0")]
 [assembly: AssemblyFileVersion("1.6.0.0")]
 
-[assembly: CLSCompliant(true)]
+[assembly: CLSCompliant(false)]
