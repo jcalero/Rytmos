f169e6e0bec6e54d571f83169adc8e9742b12c03
Jakob Calero <jakob.calero@gmail.com>
2012-07-26 16:51:29 +0000

    New loading screen.

--- Assets/Scripts/GUI/LoadingUI.cs
+++ Assets/Scripts/GUI/LoadingUI.cs
@@ -3,12 +3,23 @@ using System.Collections;
 
 public class LoadingUI : MonoBehaviour {
 	
-	public UILabel LoadingLabel;
+	public UILabel SongLabel;
+	public UISlider ProgressBar;
+	public UILabel LoadingTextLabel;
+
+	private bool SongTitleSet;
+	private string[] bullshitText;
 
 	// Use this for initialization
 	void Start () {
 
-		LoadingLabel.text = "";
+		bullshitText = new string[5];
+		bullshitText[0] = "Copying Audiosurfs idea...";
+		bullshitText[1] = "Hand drawing audio form data...";
+		bullshitText[2] = "Assigning notes to color spectrum...";
+		bullshitText[3] = "Mishmashing peaks with other stuff...";
+		bullshitText[4] = "Humming to the tune...";
+
 		//LoadingLabel.text = "[FDD017]" + "0";
 		// Check whether we want to load in a song (Game.Song is set) or use one which has been provided as an asset
 		if (Game.Song != null && Game.Song != "") {
@@ -22,12 +33,35 @@ public class LoadingUI : MonoBehaviour {
 //			}
 //		}	
 	}
+
+	//void IEnumerate() 
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (!SongTitleSet && AudioManager.tagDataSet) {
+			SongLabel.text = AudioManager.artist + " - " + AudioManager.title;
+			//CalculateSongLabelSize();
+			SongTitleSet = true;
+		}
+
 		if(!AudioManager.isSongLoaded()) {
-			LoadingLabel.text = "[FDD017]" + AudioManager.loadingProgress;	
+			ProgressBar.sliderValue = AudioManager.loadingProgress;
+			if (AudioManager.loadingProgress > 0.8) {
+				LoadingTextLabel.text = bullshitText[4];
+			} else if (AudioManager.loadingProgress > 0.6) {
+				LoadingTextLabel.text = bullshitText[3];
+			} else if (AudioManager.loadingProgress > 0.4) {
+				LoadingTextLabel.text = bullshitText[2];
+			} else if (AudioManager.loadingProgress > 0.2) {
+				LoadingTextLabel.text = bullshitText[1];
+			} else {
+				LoadingTextLabel.text = bullshitText[0];
+			}
 		}
 		else Application.LoadLevel("Game");
 	}
+	//void CalculateSongLabelSize() {
+	//    if (SongLabel.text.Length > 
+	//}
 }
