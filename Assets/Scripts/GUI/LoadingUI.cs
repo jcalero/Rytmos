c8f91f1220ecb1ce4325a17406c97a2a7f2848b0
Jakob Calero <jakob.calero@gmail.com>
2012-08-22 12:27:49 +0000

    Load screen is now fully dynamic in size for all supported resolutions. Wee!

--- Assets/Scripts/GUI/LoadingUI.cs
+++ Assets/Scripts/GUI/LoadingUI.cs
@@ -6,11 +6,17 @@ public class LoadingUI : MonoBehaviour {
 
 	public UILabel SongLabel;
 	public UISlider ProgressBar;
+	public UISlicedSprite ProgressBarBackground;
 	public UILabel LoadingTextLabel;
 	public UILabel ConfirmTextLabel;
 	public UILabel AbortedTextLabel;
 	public UISprite WaveForm1;
 	public UISprite WaveForm2;
+	public UISlicedSprite WaveFormBorder;
+	public UISlicedSprite WaveFormMidLine;
+	public GameObject WaveFormLeftNrs;
+	public GameObject WaveFormRightNrs;
+	public UIPanel WaveFormPanel;
 	public UIPanel LoadingPanel;
 	public UIPanel ReadyToPlayPanel;
 	public UIPanel ConfirmPanel;
@@ -25,13 +31,74 @@ public class LoadingUI : MonoBehaviour {
 	private int currentText = 0;
 	private List<int> usedStrings;
 	private bool isConfirming;
+	private float waveFormSwitchPoint = -661;
+	private float waveFormResetPoint = 670;
 
 	// Use this for initialization
 	void Start() {
-		
+
+		float aspect = Camera.mainCamera.aspect;
+		Debug.Log(aspect);
+
+		// Set up the loading bar and waveform for different aspect ratios
+		// Aspect ratio: 1.333..
+		if (aspect == 640.0f / 480.0f) {
+			Debug.Log("Setting up UI for aspect ratio: " + aspect);
+			ProgressBarBackground.transform.localScale = new Vector2(600, ProgressBarBackground.transform.localScale.y);
+			ProgressBar.transform.localPosition = new Vector2(-300, ProgressBar.transform.localPosition.y);
+			WaveFormBorder.transform.localScale = new Vector2(520, WaveFormBorder.transform.localScale.y);
+			WaveFormMidLine.transform.localScale = new Vector2(509, WaveFormMidLine.transform.localScale.y);
+			WaveFormLeftNrs.transform.localPosition = new Vector2(70, 0);
+			WaveFormRightNrs.transform.localPosition = new Vector2(-70, 0);
+			WaveFormPanel.clipRange = new Vector4(WaveFormPanel.clipRange.x, WaveFormPanel.clipRange.y, 511, WaveFormPanel.clipRange.w);
+			SongLabel.lineWidth = 600;
+		}
+			// Aspect ratio: 1.5
+		else if (aspect == (480.0f / 320.0f)) {
+			Debug.Log("Setting up UI for aspect ratio: " + aspect);
+			ProgressBarBackground.transform.localScale = new Vector2(650, ProgressBarBackground.transform.localScale.y);
+			ProgressBar.transform.localPosition = new Vector2(-325, ProgressBar.transform.localPosition.y);
+			WaveFormBorder.transform.localScale = new Vector2(650, WaveFormBorder.transform.localScale.y);
+			WaveFormMidLine.transform.localScale = new Vector2(636, WaveFormMidLine.transform.localScale.y);
+			WaveFormLeftNrs.transform.localPosition = new Vector2(15, 0);
+			WaveFormRightNrs.transform.localPosition = new Vector2(-15, 0);
+			WaveFormPanel.clipRange = new Vector4(WaveFormPanel.clipRange.x, WaveFormPanel.clipRange.y, 638, WaveFormPanel.clipRange.w);
+		}
+			// Aspect ratio: 1.6
+		else if (aspect == (1280.0f / 800.0f)) {
+			Debug.Log("Setting up UI for aspect ratio: " + aspect);
+			ProgressBarBackground.transform.localScale = new Vector2(650, ProgressBarBackground.transform.localScale.y);
+			ProgressBar.transform.localPosition = new Vector2(-325, ProgressBar.transform.localPosition.y);
+			WaveFormBorder.transform.localScale = new Vector2(650, WaveFormBorder.transform.localScale.y);
+			WaveFormMidLine.transform.localScale = new Vector2(636, WaveFormMidLine.transform.localScale.y);
+			WaveFormLeftNrs.transform.localPosition = new Vector2(15, 0);
+			WaveFormRightNrs.transform.localPosition = new Vector2(-15, 0);
+			WaveFormPanel.clipRange = new Vector4(WaveFormPanel.clipRange.x, WaveFormPanel.clipRange.y, 638, WaveFormPanel.clipRange.w);
+		}
+			// Aspect ratio: 1.666.. (Default android)
+		else if (aspect == (800.0f / 480.0f)) {
+			Debug.Log("Setting up UI for aspect ratio: " + aspect);
+			ProgressBarBackground.transform.localScale = new Vector2(750, ProgressBarBackground.transform.localScale.y);
+			ProgressBar.transform.localPosition = new Vector2(-375, ProgressBar.transform.localPosition.y);
+		}
+			// Aspect ratio: 1.7066..
+		else if (aspect == (1024.0f / 600.0f)) {
+			// Using the same as 1.666.. (Default)
+		}
+			// Aspect ratio: 1.779166..
+		else if (aspect == (854.0f / 480.0f)) {
+			// Using the same as 1.666.. (Default)
+		}
+			// Aspect ratio: 1.8
+		else if (aspect == (432.0f / 240.0f)) {
+			// Using the same as 1.666.. (Default)
+		}
+		ProgressBar.fullSize = new Vector2(ProgressBarBackground.transform.localScale.x - 4, ProgressBarBackground.transform.localScale.y - 2);
+		ProgressBar.foreground.transform.localScale = ProgressBar.fullSize;
+
 		//GameObject cam = GameObject.Find("Camera");
 		//cam.GetComponentInChildren<Camera>().orthographicSize = Game.cameraScaleFactor;	
-		
+
 		BSText = new string[] {
 			"Splitting Channels",
 			"Charging Spectral Flux Capacitors",
@@ -105,7 +172,7 @@ public class LoadingUI : MonoBehaviour {
 			CalculateSongLabelSize();
 			SongTitleSet = true;
 		}
-		
+
 		threeDotsTimer += Time.deltaTime;
 
 		if (!AudioManager.isSongLoaded()) {
@@ -115,11 +182,10 @@ public class LoadingUI : MonoBehaviour {
 			if (AudioManager.loadingProgress >= TextIterator / 5f && AudioManager.loadingProgress < (TextIterator + 1) / 5f) {
 
 				string baseString = BSText[currentText];
-				
-				if (threeDotsTimer < DOTSPEED) {}
-				else if (threeDotsTimer < DOTSPEED*2f) baseString += ".";
-				else if (threeDotsTimer < DOTSPEED*3f) baseString += "..";
-				else if (threeDotsTimer < DOTSPEED*4f) baseString += "...";
+
+				if (threeDotsTimer < DOTSPEED) { } else if (threeDotsTimer < DOTSPEED * 2f) baseString += ".";
+				else if (threeDotsTimer < DOTSPEED * 3f) baseString += "..";
+				else if (threeDotsTimer < DOTSPEED * 4f) baseString += "...";
 				else threeDotsTimer = 0f;
 
 				LoadingTextLabel.text = baseString;
@@ -135,10 +201,10 @@ public class LoadingUI : MonoBehaviour {
 
 
 
-			if (WaveForm1.transform.localPosition.x < -661)
-				WaveForm1.transform.localPosition = new Vector2(670, WaveForm1.transform.localPosition.y);
-			if (WaveForm2.transform.localPosition.x < -661)
-				WaveForm2.transform.localPosition = new Vector2(670, WaveForm2.transform.localPosition.y);
+			if (WaveForm1.transform.localPosition.x < waveFormSwitchPoint)
+				WaveForm1.transform.localPosition = new Vector2(waveFormResetPoint, WaveForm1.transform.localPosition.y);
+			if (WaveForm2.transform.localPosition.x < waveFormSwitchPoint)
+				WaveForm2.transform.localPosition = new Vector2(waveFormResetPoint, WaveForm2.transform.localPosition.y);
 
 			stepTimer += (15 * Time.deltaTime);
 
@@ -185,10 +251,20 @@ public class LoadingUI : MonoBehaviour {
 	}
 
 	void CalculateSongLabelSize() {
-		if (SongLabel.text.Length > 38) {
-			SongLabel.transform.localScale = new Vector2(38, 38);
-		} else if (SongLabel.text.Length > 28) {
-			SongLabel.transform.localScale = new Vector2(50, 50);
+		int textLengthLong = 38;
+		int textLengthShort = 28;
+		int longSize = 38;
+		int shortSize = 50;
+		if (Camera.mainCamera.aspect < 1.5) {
+			textLengthLong = 20;
+			textLengthShort = 10;
+			longSize = 30;
+			shortSize = 45;
+		}
+		if (SongLabel.text.Length > textLengthLong) {
+			SongLabel.transform.localScale = new Vector2(longSize, longSize);
+		} else if (SongLabel.text.Length > textLengthShort) {
+			SongLabel.transform.localScale = new Vector2(shortSize, shortSize);
 		}
 	}
 }
